ba3209ce5a8b9029b6fa969afec19cfb408a9b50
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-11 15:17:57 +0600

    add html and javscript

---
+++ index.js
@@ -0,0 +1,9 @@
+var link = document.getElementsByTagName("a")[0];
+
+link.innerHTML="facebook";
+link.style.textDecoration = "none";
+link.style.color = "red";
+link.style.fontFamily ="arial black";
+link.style.textTransform = "uppercase";
+link.style.textAlign = "center";
+link.href = "https://www.facebook.com/ahsanul.huq.984";
